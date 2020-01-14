b0216ff602e557de7e782c527e70f5e58025c5b5
DmitriChe <juneority@mail.ru>
2020-01-14 04:33:12 +0300

    final commit

--- bsoup_parser.py
+++ bsoup_parser.py
@@ -12,17 +12,16 @@ html_doc = requests.get(url).text
 soup = BeautifulSoup(html_doc, 'html.parser')
 # print(soup.prettify())
 max_page_num = max([int(num.text) for num in soup.find_all('a', class_='num')])
-print(max_page_num)
+print(f'number of pages: {max_page_num}')
 
 icos_data = {}
 
 for i in range(max_page_num):
     sleep(1)
     next_page_url = f'{url}page={i + 1}'
-    print(next_page_url)
+    print(f'current page url: {next_page_url}\n')
     html_doc = requests.get(next_page_url).text
     soup = BeautifulSoup(html_doc, 'html.parser')
-    # ico_list = soup.find('div', class_='ico_list').find_all_next('div', class_='content')
     ico_list = soup.find('div', class_='ico_list').find_all_next('tr')[1:-1]
     for item in ico_list:
         ico_name = item.find('div', class_='content').a.text
@@ -39,8 +38,8 @@ for i in range(max_page_num):
             'end_date': ico_end_date,
             'rating': ico_rating
         }
-        print(f'\n{ico_name}: {ico_link}\n{ico_description}')
-        print(f'\nstart date: {ico_start_date}\nend date: {ico_end_date}\nrating: {ico_rating}')
+        print(f'{ico_name}: {ico_link}\n{ico_description}')
+        print(f'start date: {ico_start_date}\nend date: {ico_end_date}\nrating: {ico_rating}\n')
 
 with open('ico_data.json', 'w', encoding='utf-8') as f:
     f.write(json.dumps(icos_data))
